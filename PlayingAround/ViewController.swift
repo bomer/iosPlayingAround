95004e8db6a763fcbfd57ea240a26983235a8d1e
James O'Toole <jimmeyotoole@gmail.com>
2019-06-20 11:16:16 +1000

    Initial Commit

---
+++ PlayingAround/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingAround
+//
+//  Created by James O'Toole on 20/6/19.
+//  Copyright © 2019 NoMoss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
